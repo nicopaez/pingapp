dd4956c854cd013a49305a6972f2d29f94ecf114
Nicolas Paez <nicopaez@gmail.com>
2018-03-23 20:57:18 -0300

    Adjusment to run with OpenShift Docker Ruby image

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 require 'date'
 require 'json'
+require 'socket'
 
-VERSION = '20171107.0'
+VERSION = '20180323.0'
 
 get '/' do
-  result = { :version => VERSION, :ping => DateTime.now.to_s }
+  result = { :version => VERSION, :ping => DateTime.now.to_s, :ip => IPSocket.getaddress(Socket.gethostname) }
   result.to_json
 end
