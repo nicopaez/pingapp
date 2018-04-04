a4fa8300eb66f1253238b1c1cfc0ff662fc28d28
Nicolas Paez <nicopaez@gmail.com>
2018-04-04 17:05:34 -0300

    App updated to read config

--- app.rb
+++ app.rb
@@ -4,7 +4,15 @@ require 'json'
 
 VERSION = '20171107.0'
 
+before do
+  content_type 'application/json'
+end
+
 get '/' do
-  result = { :version => VERSION, :ping => DateTime.now.to_s }
+  result = { :version => VERSION, :ping => DateTime.now.to_s, :config_location => ENV['CONFIG_LOCATION'] }
   result.to_json
+end
+
+get '/config' do
+  return File.read(ENV['CONFIG_LOCATION']);
 end
