25d046c5dd8469cf33deebccffc8c83fd8d88901
Nicolas Paez <nicopaez@gmail.com>
2018-04-19 10:24:02 -0300

    Secrets usage example

--- app.rb
+++ app.rb
@@ -2,17 +2,26 @@ require 'sinatra'
 require 'date'
 require 'json'
 
-VERSION = '20171107.0'
+VERSION = '20180419.1'
 
 before do
   content_type 'application/json'
 end
 
 get '/' do
-  result = { :version => VERSION, :ping => DateTime.now.to_s, :config_location => ENV['CONFIG_LOCATION'] }
+  result = { 
+    :version => VERSION, 
+    :ping => DateTime.now.to_s, 
+    :config_location => ENV['CONFIG_LOCATION'], 
+    :secrets_location => ENV['SECRETS_LOCATION']
+  }
   result.to_json
 end
 
 get '/config' do
   return File.read(ENV['CONFIG_LOCATION']);
+end
+
+get '/secrets' do
+  return File.read(ENV['SECRETS_LOCATION']);
 end
